ad9627d929fe53c70cb60ad07a835123f9961722
fastfruits <nichoias.henry1006@gmail.com>
2024-05-21 14:16:00 -0400

    JS Update

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 "use strict"
 
 const boxes = document.querySelectorAll(".box");
+const demo1 = document.getElementById("demoBtn1");
+const demo2 = document.getElementById("demoBtn2");
+const demo3 = document.getElementById("demoBtn3");
+const demo4 = document.getElementById("demoBtn4");
+
+
 
 window.addEventListener("scroll", DisplayContent);
-document.getElementById("demo").addEventListener("click", DisplayDemo);
+demo1.addEventListener("click", demoSwitch1);
+demo2.addEventListener("click", demoSwitch2);
+demo3.addEventListener("click", demoSwitch3);
+demo4.addEventListener("click", demoSwitch4);
 
 DisplayContent();
 
-function DisplayDemo(){
-    console.log("Displaying Demo");
-    this.box.classList.add("demo");
-}
-
 function DisplayContent(){
-    const TriggerBottom = (window.innerHeight /5) * 4.5;
+    const TriggerBottom = (window.innerHeight /5) * 4;
 
     boxes.forEach((box)=>{
         const topBox = box.getBoundingClientRect().top;
@@ -25,4 +29,44 @@ function DisplayContent(){
             box.classList.remove("show");
         }
     })
+}
+
+function demoSwitch1(){
+    let original = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, delectus dolores nemo eum, fugit saepe ipsam sed rerum temporibus incidunt eligendi expedita illo. Officia laboriosam, aspernatur vitae consequatur delectus expedita!";
+    if(document.getElementById("demo").textContent == ""){
+        document.getElementById("demo").textContent = original;
+    }
+    else{
+        document.getElementById("demo").textContent = "";
+    }
+}
+
+function demoSwitch2(){
+    let original = "";
+    if(document.getElementById("demo2").textContent == ""){
+        document.getElementById("demo2").textContent = original;
+    }
+    else{
+        document.getElementById("demo2").textContent = "";
+    }
+}
+
+function demoSwitch3(){
+    let original = "";
+    if(document.getElementById("demo3").textContent == ""){
+        document.getElementById("demo3").textContent = original;
+    }
+    else{
+        document.getElementById("demo3").textContent = "";
+    }
+}
+
+function demoSwitch4(){
+    let original = "";
+    if(document.getElementById("demo4").textContent == ""){
+        document.getElementById("demo4").textContent = original;
+    }
+    else{
+        document.getElementById("demo4").textContent = "";
+    }
 }
